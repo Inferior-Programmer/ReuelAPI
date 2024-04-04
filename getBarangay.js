7e6d5402a2226ef892011658a4c789b94a869fc2
Jeremy Cesista <43639805+Inferior-Programmer@users.noreply.github.com>
2024-04-05 00:27:14 +0800

    first

---
+++ getBarangay.js
@@ -0,0 +1,21 @@
+const GeoJsonGeometriesLookup = require('geojson-geometries-lookup');
+const geoJson = require('./lols2.json');
+
+function geos(lat, long) {
+
+  const glookup = new GeoJsonGeometriesLookup(geoJson)
+
+  const point = {type: "Point", coordinates: [long, lat]}
+  const containersCount = glookup.countContainers(point)
+
+  if(containersCount == 0){
+    // no barangays found
+    return null
+  }else{
+    // coordinate is inside a polygon
+    let result = glookup.getContainers(point)
+    return result
+  }
+}
+
+module.exports = { geos }
